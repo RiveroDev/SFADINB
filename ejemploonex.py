91ce3d45f0e9b2f3a7fda2561eab0d5823ef86d4
RichardRivero <Richard_armando@hotmail.com>
2020-12-07 17:00:34 -0400

    tabla formato

--- ejemploonex.py
+++ ejemploonex.py
@@ -6,8 +6,8 @@ puntero = la_conexion.cursor()
 
 
 # crear tabla Posicion
-puntero.execute( 'CREATE TABLE "Posicion" ("idP" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT ,"Ubicacion" VARCHAR(10) NOT NULL)')
+# puntero.execute( 'CREATE TABLE "Posicion" ("idP" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT ,"Ubicacion" VARCHAR(10) NOT NULL)')
 # crear tabla 
-
+puntero.execute('CREATE TABLE "Formato" ("idF"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"tipo"	VARCHAR(10) NOT NULL)')
 puntero.close()
 la_conexion.close() 
