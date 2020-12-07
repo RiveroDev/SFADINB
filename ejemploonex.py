986ed549d53cc842b7eb292856a2472a5c121d2d
RichardRivero <Richard_armando@hotmail.com>
2020-12-07 16:48:38 -0400

    creacion de BD

--- ejemploonex.py
+++ ejemploonex.py
@@ -4,7 +4,10 @@ la_conexion = BD.connect("Movie.db")
 
 puntero = la_conexion.cursor()
 
+
+# crear tabla Posicion
 puntero.execute( 'CREATE TABLE "Posicion" ("idP" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT ,"Ubicacion" VARCHAR(10) NOT NULL)')
+# crear tabla 
 
 puntero.close()
 la_conexion.close() 
