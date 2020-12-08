fd4e003758f506bf31da35c4741214fbace4e004
RichardRivero <Richard_armando@hotmail.com>
2020-12-08 12:24:02 -0400

    convirtiendo a classe creador base de datos

--- DataBaseCreat.py
+++ DataBaseCreat.py
@@ -1,32 +1,39 @@
 
-#Verifica si la base de datos existe 
+#Verifica si la base de datos existe
+# en dado cado que no creara una nueva base de datos en blanco si 
+# seleccionamos que si 
 
 import os.path as path
 import sqlite3 as sq3
 
-nameDataBase = "MovieMarket.db"  # nombre de la base de datos 
 
-def existeBaseDatos():
-    if path.exists(nameDataBase) != True:
-        return False
-    else:
-        return True
+class CreadorBD():
 
-def  crearBaseDatos():
-    existe = existeBaseDatos()
-    if existe != True:
-        print("La Base de datos existe")
-    else:
-        print("La Base de datos no Existe desea crrear una: S/N")
-        opcion = input()
-        if opcion.upper() == "S":
-            db = sq3.connect(nameDataBase)
-            db.close()
+    def __init__(self):
+        nameDataBase = "MovieMarket.db"  # nombre de la base de datos 
+
+    def existeBaseDatos(self):
+        if path.exists(nameDataBase) != True:
+            return False
         else:
-            print("Acuerdese no tiene base de dato activa")
-             
+            return True
+
+    def  crearBaseDatos(self,estado):
+        existe = estado
+        if existe == True:
+            print("La Base de datos existe")
+        else:
+            print("La Base de datos no Existe desea crrear una: S/N")
+            opcion = input()
+            if opcion.upper() == "S":
+                db = sq3.connect(nameDataBase)
+                db.close()
+            else:
+                print("Acuerdese no tiene base de dato activa")
 
 
+estado = existeBaseDatos()
+crearBaseDatos(estado)
     
 
 
